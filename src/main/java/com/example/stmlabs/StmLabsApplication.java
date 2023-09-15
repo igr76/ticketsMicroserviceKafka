87e25aef73b6e00e263b22febc66932ad596794b
igr76 <gritsuk766@gmail.com>
2023-09-16 08:33:51 +1000

    start

---
+++ src/main/java/com/example/stmlabs/StmLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.stmlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmLabsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StmLabsApplication.class, args);
+  }
+
+}
